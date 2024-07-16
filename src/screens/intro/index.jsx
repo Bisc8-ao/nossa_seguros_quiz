0fc58099406b1691406e44b7b0170b90b873f636
tchiinhemba <tchiinhemba@gmail.com>
2024-07-16 16:56:30 +0100

    add loren ipsun into Intro screen

--- src/screens/intro/index.jsx
+++ src/screens/intro/index.jsx
@@ -19,8 +19,12 @@ function Intro() {
     <div className="_in_wrapper">
       <Header />
       <div className="_in_container">
-        <img className="_in_nossa_logo" src={hole} alt="" />
-
+      <div className="_le_hole_container">
+          <img className="_le_nossa_logo" src={hole} alt="Logo" />
+          <p className="_le_message">
+            Lorem Ipsum Dolor Sit Amet
+          </p>
+        </div>
         <div className="_in_btn_containers">
           <MainButton text="Continuar" handleClick={handleClick} transitionDelay={500}/>
         </div>
