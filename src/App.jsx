9a5d0832b4407dfafeb6f639208c58b2099528a5
tchiinhemba <tchiinhemba@gmail.com>
2024-07-19 15:43:23 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import "./App.scss";
 import Router from "./routes/router";
-import { SoundProvider } from "./context/soundContext";
 
 function App() {
   return (
-    <>
-      <SoundProvider>
-        <Router />
-      </SoundProvider>
-    </>
+    <Router />
   );
 }
 
